e99928975c453164d355a6854ad471545aae4f87
Sonny <li.zero@yandex.ru>
2022-12-13 21:49:39 +0300

    add feedback notices

--- src/index.js
+++ src/index.js
@@ -7,26 +7,28 @@ import view from './view';
 const schema = yup.string()
   .matches(
     /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
-    'Enter correct url!',
   ).required();
 
 const state = {
   currentUrl: '',
   urls: [],
-  isValid: true,
+  isValid: 'valid',
 };
 
 const input = document.querySelector('input');
 
-const watchedState = onChange(state, (value) => {
+const watchedState = onChange(state, (path, value) => {
   schema
     .isValid(value)
     .then((valid) => {
+      console.log(valid);
       if (valid === true && !state.urls.includes(value)) {
         state.urls.push(value);
-        state.isValid = true;
+        state.isValid = 'valid';
+      } else if (state.urls.includes(value)) {
+        state.isValid = 'loaded';
       } else {
-        state.isValid = false;
+        state.isValid = 'invalid';
       }
 
       view(state);
--- src/view.js
+++ src/view.js
@@ -1,12 +1,24 @@
 const input = document.querySelector('input');
+const feedback = document.querySelector('.feedback');
 
 const view = (currentState) => {
-  console.log(currentState.isValid);
-  if (currentState.isValid === false) {
+  if (currentState.isValid === 'invalid') {
     input.classList.add('red-border');
+    input.classList.add('is-invalid');
+    feedback.classList.add('text-danger');
+    feedback.textContent = 'Ссылка должна быть валидным URL';
   }
-  if (currentState.isValid === true) {
+  if (currentState.isValid === 'valid') {
     input.classList.remove('red-border');
+    input.classList.remove('is-invalid');
+    feedback.classList.remove('text-danger');
+    feedback.classList.add('text-success');
+    feedback.textContent = 'RSS успешно загружен';
+  }
+  if (currentState.isValid === 'loaded') {
+    input.classList.remove('red-border');
+    feedback.classList.add('text-danger');
+    feedback.textContent = 'RSS уже существует';
   }
 };
 
